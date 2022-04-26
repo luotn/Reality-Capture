bdf3d1c6ebd996a7d3b46d944d207b8a6839a7f7
luotn <luotn@hotmail.com>
2022-04-26 17:38:36 +0100

    Initial Commit

---
+++ Reality Capture/Reality_CaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reality_CaptureApp.swift
+//  Reality Capture
+//
+//  Created by 罗天宁 on 26/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct Reality_CaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
